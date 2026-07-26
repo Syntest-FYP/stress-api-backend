5a9131317442af5bfedfaed9a301749080632d28
dunkel68 <crimlina68@gmail.com>
2026-07-26 23:03:44 +0500

    nil

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000, // fail fast instead of hanging
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
